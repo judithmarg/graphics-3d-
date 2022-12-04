26fdde3579fbee3e73c96d078510897b794d0ac6
judithmarg <judithmargaritapacollalli@gmail.com>
2022-12-04 14:57:26 -0400

    Codigo final

--- RayTracing.py
+++ RayTracing.py
@@ -1,21 +1,23 @@
-import cv2
+from PIL import Image
 import numpy as np
 
-def intersectarEsfera(origen,direccion):
-    radio = 6
-    centro = [4,4,4] 
+def intersectarEsfera(origen,direccion,centro,radio):
     xo,yo,zo = origen[0],origen[1],origen[2]
     xd,yd,zd = direccion[0],direccion[1],direccion[2]
+    xc,yc,zc = centro[0], centro[1],centro[2]
     ecA = xd**2 +yd**2 + zd**2
-    ecB = 2*xd*(xo-centro[0])+2*yd*(yo-centro[1])+2*zd*(zo-centro[2])
-    ecC = (xo-xd)**2+(yo-yd)**2+(zo-zd)**2-radio**2
+    ecB = 2*xd*(xo-xc)+2*yd*(yo-yc)+2*zd*(zo-zc)
+    ecC = (xo-xc)**2+(yo-yc)**2+(zo-zc)**2-radio**2
     discriminante = ecB**2-4*ecA*ecC
     if discriminante < 0 :
         return (np.inf,None) 
     else:
-        t1 = (-ecB+ np.sqrt(discriminante))/(2*ecA)
-        t2 = (-ecB- np.sqrt(discriminante))/(2*ecA)
-        hayInterseccion = True if t1 > 0 or t2 > 0 else False
+        t1 = (-ecB+ np.sqrt(discriminante))/(2.00)
+        t2 = (-ecB- np.sqrt(discriminante))/(2.00)
+        aux = t1 if ecB < 0 else t2
+        t1 = aux / ecA
+        t2 = ecC / aux  
+        hayInterseccion =  t1 > 0 or t2 > 0 
         if hayInterseccion and t1 > 0:
             pointInter = np.array(origen) + t1*np.array(direccion)
         elif hayInterseccion and t2 > 0:
@@ -24,8 +26,10 @@ def intersectarEsfera(origen,direccion):
             return (np.inf,None)
         vectorNormal = (np.subtract(pointInter,centro))/radio
         vector = [pointInter, vectorNormal]
-        t = t1 if t1 > 0 else t2
-        return (t, vector) 
+        t1, t2 = min(t1, t2), max(t1, t2)
+        if t2 >= 0:
+            t = t2 if t1 < 0 else t1
+        return (t,vector) 
 
 def iluminarDirectly(colorObjeto):
     color_amb = (0.5*colorObjeto[0], 0.5*colorObjeto[1], 0.5*colorObjeto[2])
@@ -45,9 +49,10 @@ def rayoTransmite(normal, vector):
     n21 = 1
     normalA = np.array(normal)
     vectorA = np.array(vector)
-    vectorC = np.cross(normalA, vectorA)# producto cruz en vectores
-    vectorCcuadrado = np.dot(vectorC, vectorC) #producto punto en vector c
-    direccionTransmite = n21*vectorA + (n21*vectorC - np.sqrt(1+(n21**2*(vectorCcuadrado-1))))*normalA
+    vectorC = np.cross(normalA, vectorA)
+    vectorCcuadrado = np.dot(vectorC, vectorC) 
+    direccionTransmite = n21*vectorA + (n21*vectorC 
+    - np.sqrt(1+(n21**2*(vectorCcuadrado-1))))*normalA
     return direccionTransmite
 
 def norma(vector):
@@ -55,10 +60,8 @@ def norma(vector):
     return vector
 
 def combinarContribColor(intensidad, luz_amb,l,n,v,vn):
-    kdifuso = 0.10
-    kspecular = 0.10
-    kambiente = 0.5
-    intAmb = iluminarDirectly((91, 171, 170))
+    
+    intAmb = intensidad
     r = np.subtract(2*np.array(n)*(np.dot(l,n)),l) 
     t = rayoTransmite(n,v)
     kd_red = kdifuso*luz_amb[0]
@@ -76,60 +79,102 @@ def combinarContribColor(intensidad, luz_amb,l,n,v,vn):
     kst_blue = ks_blue *1
     ka_blue = kambiente*luz_amb[2]
     blue_resultante = (intensidad[2])*(kd_blue*np.dot(l,n)+ks_blue*(np.dot(r,v)**vn)+kst_blue*(np.dot(t,v))**vn)+ka_blue*intAmb[2]
-    return (int(red_resultante),int(green_resultante),int(blue_resultante))
+    return (max(red_resultante,0),max(green_resultante,0),max(blue_resultante,0))
 
-def interseccion(vectorOrigen, vectoDireccion, objeto):
+def normalizado(obj, rayo):
+    if obj['type'] == 'esfera':
+        return norma(rayo - obj['posicion'])
+
+def get_color(obj, rayo):
+    color = obj['color']
+    if not hasattr(color, '__len__'):
+        color = color(rayo)
+    return color 
+
+def interseccion1(vectorOrigen, vectorDireccion, objeto):
     if objeto['type'] == 'esfera':
-        intersectarEsfera(vectorOrigen,vectorDireccion,objeto['pos'],objeto['normal'])
+        return intersectarEsfera(vectorOrigen,vectorDireccion,objeto['posicion'],objeto['radio'])
 
 ###################
+
+def add_esfera(posicion, radio, color):
+    return dict(type='esfera',posicion=np.array(posicion), radio=np.array(radio),color=np.array(color))
+
+escena = [add_esfera([.75,.1,1.], .6, [0.,0.,1.]),
+          add_esfera([-.75, .1, 2.25], .6, [.5, .223, .5]),
+          add_esfera([-2.75, .1, 3.5], .6, [1., .572, .184])]
+
 camara = np.array([0.,0.35,-1.])
 punteroCamara = np.array([0.,0.,0.])
 luz = np.array([5.,5.,-10.])
-ancho , alto = 300,200
+ancho , alto = 400,300
 diferencia =float(ancho)/alto 
-space = (-1., -1/(diferencia)+.25 , 1., 1./diferencia+.25)
+space = (-1., -1./(diferencia)+.25 , 1., 1./diferencia+.25)
 vectorDireccion = norma(punteroCamara - camara)
+kdifuso = 1.
+kspecular = 1.
+kambiente = 0.05
 
 def prueba():
-    blank = np.zeros((180,180,3), dtype="uint8")
-    for i in enumerate(np.linspace(space[0],space[2],180)):
-        for j in enumerate(np.linspace(space[1],space[3],180)):
-            blank[i,j] = rayTracer(camara, vectorDireccion, 0, (91, 171, 170))
-            punteroCamara[:2] = (i,j) 
+    img = np.zeros((alto,ancho,3))
+    vectorDireccion = norma(punteroCamara - camara)
+    for i,x in enumerate(np.linspace(space[0],space[2],ancho)): 
+        if i % 10 == 0:
+            print(i / float(ancho) * 100, "%")
+        for j,y in enumerate(np.linspace(space[1],space[3],alto)):
+            punteroCamara[:2] = (x,y) 
             vectorDireccion = norma(punteroCamara-camara)
-    cv2.imshow("Prueba",blank)
-    cv2.waitKey(0)
+            colorFinal = rayTracer(camara, vectorDireccion, 0, (0,0,0), escena)
+            img[alto - j - 1, i, :] = np.clip(colorFinal, 0, 1)
 
-def rayTracer(origen, direccion,profundidad,color):
-    ##interseccion con cada figura 
-    vector3D = np.array(origen) + 3*np.array(direccion)  ##cambiar vector3D a rayo
-    punto_interseccion = vector3D
-    direccion_reflejada = vector3D
-    direccion_transparente = vector3D
-    color_local,color_reflejado,color_transparente = color , color, color
-    if profundidad < 5:
+    im = Image.fromarray((255 * img).astype(np.uint8), "RGB")
+    im.save("figurita.png")
+
+def rayTracer(origen, direccion,profundidad,color,escena):
+
+    if profundidad >= 2:
         color = (0,0,0)
         return color
     else:
-        interseccion = interseccion(origen,direccion)     #####ahora funciona con una esfera
-        if interseccion[2] is None:
+        ###
+        t = np.inf
+        for i, figura in enumerate(escena):
+            t_figura = interseccion1(origen,direccion,figura)[0]
+            if t_figura < t:
+                t, figura_ind = t_figura, i 
+        if t == np.inf:
+            return (0,0,0)
+        figura = escena[figura_ind]
+        rayo = origen + t* direccion
+        rayoNormalizado = normalizado(figura, rayo)
+        paraLuz = norma(luz - rayo)
+        paraOrigen = norma(camara - rayo)
+        punto_interseccion = rayoNormalizado
+        direccion_reflejada = rayoNormalizado
+        direccion_transparente = rayoNormalizado
+        figuraColor = get_color(figura, rayo)
+        color_local,color_reflejado,color_transparente = figuraColor , figuraColor, figuraColor
+        
+        # ##
+        interseccionRes = interseccion1(origen,direccion,figura)     #####ahora funciona con una esfera
+        if interseccionRes[1] is None:
             color = (0,0,0) 
             return color
         else:
-            color_local = iluminarDirectly(color) 
-            direccion_reflejada = interseccion[1]           ### direccion donde intersecta con the shape
-            direccion_reflejada = rayoReflejado(direccion_reflejada, direccion)                 #####cambiar a direccion reflejada (?)
-            punto_interseccion = interseccion[0]             ### punto donde intersecta con la figura, cambiar (?)
-            rayTracer(punto_interseccion, direccion_reflejada,profundidad+1, color_reflejado) 
+            color_local = iluminarDirectly(figuraColor) 
+            direccion_reflejada = interseccionRes[1][1]           ### direccion donde intersecta con the shape
+            direccion_reflejada = rayoReflejado(direccion_reflejada, rayoNormalizado)                 #####cambiar a direccion reflejada (?)
+            punto_interseccion = interseccionRes[1][0]             ### punto donde intersecta con la figura, cambiar (?)
+            rayTracer(punto_interseccion, direccion_reflejada,profundidad+1, color_reflejado,escena) 
             direccion_transparente = rayoTransmite(direccion_transparente, direccion_reflejada)         #### direccion a cambiar?
-            rayTracer(punto_interseccion, direccion_transparente,profundidad+1,color_transparente)
-            colorFin = combinarContribColor(color_local,color_local,direccion_reflejada,direccion_reflejada,direccion_transparente,1)
-    return colorFin
-
+            rayTracer(punto_interseccion, direccion_transparente,profundidad+1,color_transparente,escena)
+            color = combinarContribColor(figuraColor,color_local,[1,1,1],direccion_reflejada,direccion_transparente,1)
+            
+    return color
 
 def main():
     prueba()
+
 if __name__ == '__main__':
     main()
 
